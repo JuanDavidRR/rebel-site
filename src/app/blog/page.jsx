11f063c38e870cbed279abe4f6afaed7d20ec6b2
JuanelCJ <juandavidrom@outlook.com>
2023-08-08 15:12:57 -0500

    Fixing some issues

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
+export const dynamic = 'force-dynamic'
 import { seoSeparator, seoTitle } from "@/utils/constants";
 
 export const metadata = {
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/button/Button";
+export const dynamic = 'force-dynamic'
 import { seoSeparator, seoTitle } from "@/utils/constants";
 
 export const metadata = {
@@ -18,13 +19,13 @@ const Contact = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43180.295324142615!2d-122.31508907708233!3d37.546066413995874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f9e70368acbe7%3A0x837ed9639285d6fb!2sSede%20de%20Roblox!5e0!3m2!1ses-419!2sco!4v1691515062772!5m2!1ses-419!2sco"
             width="100%"
             height="450"
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
 
-        <form className={styles.form} action="/" method="post">
+        <form className={styles.form}>
           <div className={styles.formItem}>
             <label htmlFor="name">Full Name</label>
             <input
