9e84710dfb8c91311990e42e83343d97bfa21412
JuanelCJ <juandavidrom@outlook.com>
2023-08-08 21:11:18 -0500

    Changing .env file

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 export const dynamic = 'force-dynamic'
 import { seoSeparator, seoTitle } from "@/utils/constants";
+import styles from "./page.module.css";
 
 export const metadata = {
   title: 'Blog' + seoSeparator + seoTitle,
